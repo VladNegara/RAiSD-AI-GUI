03968501719415bdf8b1e7525ede26241065e3d4
Vlad Negara <150728638+VladNegara@users.noreply.github.com>
2026-02-26 23:30:08 +0100

    Refactor ParameterGroupList

--- gui/model/ParameterGroupList.py
+++ gui/model/ParameterGroupList.py
@@ -1,19 +1,21 @@
+from typing import Self
+
 from model.ParameterGroup import ParameterGroup
 from model.Parameter import Parameter
 
 class ParameterGroupList():
-    def __init__(self, config_file_path:str):
-        self.config_file_path = config_file_path
-        self.parameter_groups = []
-        self.generate_from_configuration_file()
+    def __init__(self, parameter_groups: list[ParameterGroup] | None = None):
+        self._parameter_groups = parameter_groups or []
 
-    def add_parameter_group(self, parameter_group:ParameterGroup):
-        self.parameter_groups.append(parameter_group)
-
-    def get_parameter_groups(self) -> list[ParameterGroup]:
-        return self.parameter_groups
-    
-    def generate_from_configuration_file(self):
+    @classmethod
+    def from_configuration_file(cls, file_path: str) -> Self:
         # TODO: Implement this method: make parameter_groups, add parameters, add groups to set
-        # use self.config_file_path
-        pass
+        parameter_groups = []
+        return cls(parameter_groups)
+
+    @property
+    def parameter_groups(self) -> list[ParameterGroup]:
+        return self._parameter_groups
+
+    def add_parameter_group(self, parameter_group: ParameterGroup):
+        self._parameter_groups.append(parameter_group)
