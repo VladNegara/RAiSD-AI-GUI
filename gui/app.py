79b509889981fe5c1ab31522a2e11417e594b714
Vlad Negara <150728638+VladNegara@users.noreply.github.com>
2026-02-26 23:04:07 +0100

    Rename "parameter group set" to "list"

--- gui/app.py
+++ gui/app.py
@@ -2,16 +2,16 @@ import sys
 
 from PySide6.QtWidgets import QApplication, QStyleFactory
 
-from model.ParameterGroupSet import ParameterGroupSet
+from gui.model.ParameterGroupList import ParameterGroupList
 from windows.main import MainWindow
 
 def main():
     app = QApplication(sys.argv)
     QApplication.setStyle(QStyleFactory.create("windowsvista"))
 
-    parameter_group_set = ParameterGroupSet("gui/...") # TODO: implement config file and write path
+    parameter_group_list = ParameterGroupList("gui/...") # TODO: implement config file and write path
 
-    window = MainWindow(parameter_group_set=parameter_group_set)  
+    window = MainWindow(parameter_group_list=parameter_group_list)  
     window.show()
     app.exec()
     print("App closed")
