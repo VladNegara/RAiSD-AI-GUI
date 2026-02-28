b7c0a468d5a423b9592bd32010eeea05c6f365ca
Vlad Negara <150728638+VladNegara@users.noreply.github.com>
2026-02-28 17:04:26 +0100

    Document Parameter class

--- gui/model/Parameter.py
+++ gui/model/Parameter.py
@@ -15,6 +15,14 @@ class AbstractQObjectMeta(type(ABC), type(QObject)):
 
 
 class Parameter(ABC, QObject, Generic[T], metaclass=AbstractQObjectMeta):
+    """
+    A base class for parameters to be filled in using the GUI.
+
+    The class inherits from `ABC` to make it abstract, from `QObject`
+    to use the signal mechanism and from `Generic` to add type hints
+    based on the type of value that the parameter stores.
+    """
+
     value_changed: Signal
 
     def __init__(
@@ -22,6 +30,21 @@ class Parameter(ABC, QObject, Generic[T], metaclass=AbstractQObjectMeta):
             name: str, description: str, flag: str,
             default_value: T,
     ) -> None:
+        """
+        Initialize a `Parameter` object.
+
+        :param name: the name of the parameter
+        :type name: str
+
+        :param description: a longer description of the parameter
+        :type description: str
+
+        :param flag: the command-line flag of the parameter
+        :type flag: str
+
+        :param default_value: the default value of the parameter
+        :type default_value: T
+        """
         super().__init__(self)
         self.name = name
         self.description = description
@@ -31,6 +54,9 @@ class Parameter(ABC, QObject, Generic[T], metaclass=AbstractQObjectMeta):
 
     @property
     def value(self) -> T:
+        """
+        The current value of the parameter.
+        """
         return self._value
 
     @value.setter
@@ -39,12 +65,25 @@ class Parameter(ABC, QObject, Generic[T], metaclass=AbstractQObjectMeta):
         self.value_changed.emit(self.value, self.valid)
 
     def reset_value(self) -> None:
+        """
+        Reset the value of the parameter to the default value.
+        """
         self._value = self.default_value
 
     @property
     def valid(self) -> bool:
+        """
+        Whether the current value of the parameter is valid.
+        """
         return True
 
     @abstractmethod
     def to_cli(self) -> str:
+        """
+        Represent the parameter for the command line, taking into
+        account its current value.
+
+        :return: the command-line representation
+        :rtype: str
+        """
         pass
