05354fde1060024fe164c7a7a13d8e4aafeb3997
Vlad Negara <150728638+VladNegara@users.noreply.github.com>
2026-03-22 13:22:44 +0100

    Document operation module

--- gui/model/operation.py
+++ gui/model/operation.py
@@ -1,3 +1,7 @@
+"""
+A module containing the `Operation` class.
+"""
+
 from dataclasses import dataclass
 
 from gui.model.file_structure import FileStructure
@@ -5,6 +9,13 @@ from gui.model.file_structure import FileStructure
 
 @dataclass
 class Operation():
+    """
+    A data class which holds the details of an operation.
+
+    This class is used to hold intermediate information about operations
+    between parsing and building the operation tree.
+    """
+
     id: str
     name: str
     description: str
