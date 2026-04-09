1ccbf6eeeb781f8ef397e5df24d69bdf6204573a
parrot <s.h.broeder@student.utwente.nl>
2026-04-09 08:51:50 +0000

    Create outline of RunRecord tests

---
+++ gui/tests/unit/model/test_run_record.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+class TestRunRecord:
+    """Tests for RunRecord class."""
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        pass
+
+    def test_init_values(self):
+        """Test RunRecord."""
+        # TODO: Implement this testing class
+        # Arrange
+
+        # Act
+
+        # Assert
+        pytest.skip()
