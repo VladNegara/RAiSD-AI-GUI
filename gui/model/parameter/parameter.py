53529922d966b3c97056673d64ea0d692305f1ba
giulica25 <150700518+giulica25@users.noreply.github.com>
2026-04-13 16:59:59 +0200

    Apply suggestion from @Steef-Broeder
    Co-authored-by: Steef Broeder <76819071+Steef-Broeder@users.noreply.github.com>

--- gui/model/parameter/parameter.py
+++ gui/model/parameter/parameter.py
@@ -863,7 +863,6 @@ class StringTableParameter(Parameter[tuple[()]]):
             row = []
             for _, default_value, constraints in columns:
                 parameter = StringParameter(
-                    # TODO: make these None when the rest supports it
                     name="",
                     description="",
                     flag="",
