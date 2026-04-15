91243cd6d11a78ef13edce7d7b883b122e664e70
alphanmete <alphanmete22@gmail.com>
2026-04-15 15:04:45 +0200

    Clarify license version

--- gui/pages/settings/settings_page.py
+++ gui/pages/settings/settings_page.py
@@ -217,8 +217,7 @@ class SettingsPage(Page):
             <b>Taylan Kıncır</b>, <b>Alphan Mete</b>, <b>Vlad Negară</b> and <b>Giulia Tălău</b>.<br>
             
             <br>This program is free software; you may redistribute it and/or modify its under the terms of the 
-            GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or 
-            (at your option) any later version.<br>
+            GNU General Public License version 2.0 as published by the Free Software Foundation. <br>
 
             <br>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
             even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
