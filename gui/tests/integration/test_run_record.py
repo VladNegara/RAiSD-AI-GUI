cf697e1976518b78b8d5674d78186b2af25b2a40
Loes Baart de la Faille <l.baartdelafaille@student.utwente.nl>
2026-04-16 20:50:46 +0000

    Improve validity test

--- gui/tests/integration/test_run_record.py
+++ gui/tests/integration/test_run_record.py
@@ -11,14 +11,12 @@ from gui.model.operation.file_structure import SingleFile
 from gui.model.run_record import RunRecord
 from gui.model.parameter import (
     IntervalConstraint,
-    MaxLengthConstraint,
     RegexConstraint,
     ParameterGroup,
     OptionalParameter,
     MultiParameter,
     BoolParameter,
     IntParameter,
-    FloatParameter,
     EnumParameter,
     StringParameter,
     FileParameter,
@@ -31,66 +29,83 @@ class TestParameterStructures:
 
     @fixture(autouse=True)
     def set_run_record(self):
-        self.run_id_parameter = StringParameter (
+        # Parameters
+        self.run_id_parameter = StringParameter(
+            name='name',
+            description='description',
+            flag='-f ',
+            operations={'IMG-GEN'},
+            default_value='default',
+            constraints=[
+                RegexConstraint(
+                    pattern=re.compile(r"\b[a-z]+\b"),
+                    hint="Only lowercase letters.",
+                ),
+            ],
+        )
+        self.enum_parameter = EnumParameter (
             name="Run ID",
             description="Fill in a name to identify your run.",
             flag="-n",
             operations={"IMG-GEN", "MDL-GEN"},
-            default_value="my_run",
-            constraints=[
-                  RegexConstraint(
-                      pattern=re.compile(r"[^!]"),
-                      hint="No exclamation marks!",
+            options=[("name1", "1"),("name2","2")],
+            default_value=0,
+        )
+        self.optional_parameter = OptionalParameter(
+            name='optional',
+            description='This is optional',
+            operations={'MDL-GEN'},
+            default_value=False,
+            parameter=IntParameter(
+              name='int',
+              description='description',
+              flag='-i ',
+              operations={'MDL-GEN'},
+              default_value=3,
+              constraints=[
+                  IntervalConstraint(
+                      lower_bound=2,
+                      lower_bound_inclusive=False,
+                      upper_bound=7,
+                      upper_bound_inclusive=False,
                   ),
               ],
+          )
         )
-        self.string_parameter = StringParameter(
-                        name='name',
-                        description='description',
-                        flag='-f ',
-                        operations={'IMG-GEN'},
-                        default_value='default',
-                        constraints=[
-                            RegexConstraint(
-                                pattern=re.compile(r"\b[a-z]+\b"),
-                                hint="Only lowercase letters.",
-                            ),
-                        ],
-                    )
-        self.optional_parameter = OptionalParameter(
-                        name='optional',
-                        description='This is optional',
-                        operations={'MDL-GEN'},
-                        default_value=False,
-                        parameter=IntParameter(
-                          name='int',
-                          description='description',
-                          flag='-i ',
-                          operations={'MDL-GEN'},
-                          default_value=3,
-                          constraints=[
-                              IntervalConstraint(
-                                  lower_bound=2,
-                                  lower_bound_inclusive=False,
-                                  upper_bound=7,
-                                  upper_bound_inclusive=False,
-                              ),
-                          ],
-                      )
-                    )
-        self.parameters: Sequence[Parameter] = [
-                    self.string_parameter,
-                    self.optional_parameter
-                ]
+        self.file_parameter = FileParameter (
+            name="file",
+            description="file",
+            flag="-fi ",
+            operations={"IMG-GEN"}
+        )
+        self.multi_parameter = MultiParameter ( #type: ignore
+                    name="multi",
+                    description="descr",
+                    flag="-m ",
+                    operations={'IMG-GEN'},
+                    parameters=[self.file_parameter]
+                )
+        
+        # Parameter groups
+        self.parameters_group1: Sequence[Parameter] = [
+            self.multi_parameter
+        ]
+        self.parameters_group2: Sequence[Parameter] = [
+            self.enum_parameter,
+            self.optional_parameter
+        ]
         self.parameter_groups = [
             ParameterGroup(
                 name='img',
-                parameters=[]), 
+                parameters=self.parameters_group1 # type: ignore
+                ), 
             ParameterGroup(
                 name='mdl',
-                parameters=self.parameters # type: ignore
+                parameters=self.parameters_group2 # type: ignore
             ),
         ]
+        
+        # Operations
         self.operations = {
             "MDL-GEN": Operation(
                 id="mdl",
@@ -142,31 +157,46 @@ class TestParameterStructures:
         self.categorized_operation_trees = [
             ('Operations', self.operation_trees),
         ]
+
+        # RunRecord
         self.run_record = RunRecord(
             run_id_parameter=self.run_id_parameter,
             categorized_operation_trees=self.categorized_operation_trees,
             parameter_groups=self.parameter_groups,
         )
 
-    def test_valid_based_on_params(self):
+    def test_valid_based_on_params(self, tmp_path):
         """Test whether the validity of parameters is passed along from 
         Parameter to ParameterGroup to RunRecord correctly."""
         # Arrange
         record = self.run_record
+        file = tmp_path / "file"
+        file.write_text("")
 
         # Act and Assert
-        assert record.valid
-        self.string_parameter.value = "HI"
         assert not record.valid
-        self.string_parameter.reset_value()
+        self.file_parameter.value = [tmp_path / "file"]
+        assert self.run_id_parameter.valid
+        assert self.file_parameter.valid
+        assert self.enum_parameter.valid
+        assert self.multi_parameter.valid
+        assert record.valid
+
+        assert record.valid
+        self.run_id_parameter.value = "HI"
+        assert not record.valid
+        self.run_id_parameter.reset_value()
 
         self.optional_parameter.parameter.value = 0
         assert not record.valid
         self.optional_parameter.parameter.reset_value()
 
-        self.run_id_parameter.value = "!"
-        assert not record.valid
-        self.run_id_parameter.reset_value()
+        self.file_parameter.reset_value()
+
+
+    def test_parameters_getter(self):
+        """Test if the parameters property of the RunRecord correctly combines
+        the parameters from the parameter groups."""
         
     def test_to_cli(self):
         # arrange
